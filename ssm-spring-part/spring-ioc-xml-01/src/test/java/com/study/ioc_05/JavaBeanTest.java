7ee92a1332160e5dd0f776befc92dcaec32db37d
htLiang <l15304704058@126.com>
2026-03-07 13:27:27 +0800

    test(ioc): 添加测试验证IOC容器初始化与销毁方法触发
    - 编写JavaBeanTest类用于测试IOC容器配置
    - 在initTest方法中创建ClassPathXmlApplicationContext容器实例
    - 验证容器初始化时组件实例化与init方法调用
    - 调用close方法触发容器销毁和destroy方法执行
    - 整体测试确保IOC容器生命周期正确管理

---
+++ ssm-spring-part/spring-ioc-xml-01/src/test/java/com/study/ioc_05/JavaBeanTest.java
@@ -0,0 +1,35 @@
+package com.study.ioc_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ * ClassName: Practice01
+ * Package: java001.day05
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/9/27 14:19
+ * {@code @Version} 1.0
+ */
+
+public class JavaBeanTest {
+	/**
+	* 测试ioc配置的初始化和销毁方法的触发
+	*/
+
+	@Test
+	public void initTest(){
+		//1.创建ioc容器，就会自动进行组件对象的实例化 -> init()方法
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring-05.xml");
+
+		//ioc -> 容器去调用destroy
+		// ioc 会立即释放，死了
+
+		// 2. 正常结束ioc容器
+		classPathXmlApplicationContext.close();
+
+	}
+}
