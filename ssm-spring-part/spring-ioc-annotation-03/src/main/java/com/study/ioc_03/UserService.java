63d7700caf3165d3b089ddad565682bdb0a65dc1
htLiang <l15304704058@126.com>
2026-03-10 20:05:44 +0800

    feat(ioc): 新增基础JavaBean和Spring配置文件模板
    - 新建JavaBean类作为基础结构，包含文件头注释
    - 新建UserController类，含作者和版本注释
    - 新建UserService接口，包含基本框架和注释
    - 新建UserServiceImpl实现类，完善结构注释
    - 添加spring-02.xml和spring-03.xml空白Spring配置文件模板

---
+++ ssm-spring-part/spring-ioc-annotation-03/src/main/java/com/study/ioc_03/UserService.java
@@ -0,0 +1,11 @@
+package com.study.ioc_03;
+
+/**
+ * ClassName: Practice01
+ * Package: java001.day05
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/9/27 14:19
+ * {@code @Version} 1.0
+ */public interface UserService {
+}
