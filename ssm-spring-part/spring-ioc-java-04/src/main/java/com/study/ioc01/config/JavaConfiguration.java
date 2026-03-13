f8876ff42149e6cd22f3d01921103b4a4128ee6c
htLiang <l15304704058@126.com>
2026-03-13 10:41:39 +0800

    chore(deps): 调整项目依赖与配置优化
    - 在ssm-spring-part模块的pom.xml中增加spring-jdbc依赖
    - 移除spring-ioc-annotation-03模块和spring-practice模块中的druid及spring-jdbc依赖
    - 在JavaConfiguration配置类中添加JdbcTemplate的@Bean方法，支持数据源注入
    - 详细注释@Bean方法的命名、生命周期及依赖注入方式
    - 修改.gitignore，删除多余空行和未使用资源目录的过滤规则

--- ssm-spring-part/spring-ioc-java-04/src/main/java/com/study/ioc01/config/JavaConfiguration.java
+++ ssm-spring-part/spring-ioc-java-04/src/main/java/com/study/ioc01/config/JavaConfiguration.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -40,19 +41,31 @@ public class JavaConfiguration {
 	private String username;
 	@Value("${practice.password}")
 	private String password;
+
 	/**
 	 * <bean -> 对应着一个方法
 	 * <p>
 	 * 方法的返回值类型= bean组件的类型或者是他的接口和父类
 	 * 方法的名字 = bean 的ID
 	 * 方法提可以自定义实现过程即可
-	 *
+	 * <p>
 	 * 最重要的一步：添加@Bean，会真正让配置类的方法创建的组件存储到ioc容器中
+	 * <p>
+	 * 问题一： beanName问题：
+	 *          默认：方法名
+	 *          指定：name/value属性起名字，覆盖方法名
+	 * 问题二： 周期方法如何指定
+	 *          原有注解方案：postConstruct/PreDestory 注解指定
+	 *          bean属性指定：initMethod / destroyMethod 指定
+	 * 问题三： 作用域：
+	 *          和之前还是一样@Scope注解，默认是单例
+	 * <p>
+	 * 问题四：如何引用其他的ioc容器
+	 *          直接调用对方的bean 方法即可
+	 *          直接形参变量进行引入，要求必须有对应的组件，如果有多个，形参名 = 组件id标识即可
 	 */
-	@Bean
+	@Bean(name = "ergouzi", initMethod = "", destroyMethod = "")
 	public DataSource dataSource() {
-
-
 		//实现具体的实例化过程
 		DruidDataSource dataSource = new DruidDataSource();
 		dataSource.setUrl(url);
@@ -61,4 +74,26 @@ public class JavaConfiguration {
 		dataSource.setPassword(password);
 		return dataSource;
 	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		// 需要DataSource ,也就是需要ioc容器中的其他组件
+		// 方案一：如果其他组件也是@Bean方法，可以直接调用，从ioc容器中获取组件。不推荐
+		jdbcTemplate.setDataSource(dataSource());
+		return jdbcTemplate;
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate1(DataSource dataSource) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		// 需要DataSource ,也就是需要ioc容器中的其他组件
+		// 方案二：通过形参列表声明想要的组件类型，可以是一个也可以是多个,ioc容器会自动注入
+		// 要求：
+		// 1. 必须有对应的类型的组件，如果没有就会抛出异常
+		// 2. 如果有多个类型可以使用形参名称等于对应的bean id标识即可
+		jdbcTemplate.setDataSource(dataSource());
+		return jdbcTemplate;
+	}
+
 }
