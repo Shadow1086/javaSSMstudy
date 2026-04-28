77d3cc885ef2e1699af536ab8da5aee3e4db5d68
htLiang <l15304704058@126.com>
2026-04-28 16:34:27 +0800

    ✨ feat: mybatis plus中的核心注解：@TableId,主键的生成策略，比如：自增，雪花算法，UUID等

---
+++ ssm-mybaits-plus-demo/demo02-mp-wrapper/src/test/java/com/study/MpTableIdTest.java
@@ -0,0 +1,24 @@
+package com.study;
+
+import com.study.mapper.ScheduleMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Package: com.study
+ * Description:
+ * <p>
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2026-2026/4/28 16:09
+ */
+@SpringBootTest
+public class MpTableIdTest {
+	@Autowired
+	private ScheduleMapper mapper;
+
+	@Test
+	public void test01() {
+
+	}
+}
