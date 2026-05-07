29f653f4eed4e02c57a4bc4f24e8ebc22e55cc83
htLiang <l15304704058@126.com>
2026-05-07 14:19:53 +0800

    ✨ feat: 新增websocket使用示例

---
+++ webSocket/src/test/java/com/ck/it/WebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
